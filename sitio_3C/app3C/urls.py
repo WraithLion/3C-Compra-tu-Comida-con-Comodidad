e99c99dafee629d05083d4ad6d9bd036ebb45efd
EmilioRomero <emilio_romero@ciencias.unam.mx>
2026-05-25 17:05:51 -0600

    Añadidas vistas de casos de uso Romero Concha. Plantillas y back aún sin implementar

--- sitio_3C/app3C/urls.py
+++ sitio_3C/app3C/urls.py
@@ -7,6 +7,25 @@ urlpatterns = [
     path('cliente/',views.sitioPrincipalCliente,name='sitioPrincipal_Cliente'),
     path('restaurante/',views.sitioPrincipalRestaurante,name='sitioPrincipal_Restaurante'),
     path('repartidor/',views.sitioPrincipalRepartidor,name='sitioPrincipal_Repartidor'),
-    
-    
+
+    path('crearCuenta/tipo', views.crearCuenta, name='tipoCuenta'),
+    path('crearCuenta/cliente', views.datosCliente, name='datosCliente'),
+    path('crearCuenta/repartidor', views.datosRepartidor, name='datosRepartidor'),
+    path('crearCuenta/restaurante', views.datosRestaurante, name='datosRestaurante'),
+    path('crearCuenta/tarjeta', views.datosTarjeta, name='datosTarjeta'),
+    path('crearCuenta/vehiculo', views.datosVehiculo, name='datosVehiculo'),
+
+    path('carrito/', views.carrito, name='carrito'),
+    path('pedido/direccion', views.direccionEntrega, name='direccionEntrega'),
+    path('pedido/pago', views.metodoPago, name='metodoPago'),
+    path('cliente/pedido', views.pedidoCliente, name='estadoPedidoCliente'),
+
+    path('user/modificarCuenta', views.modificarCuenta, name='modificarCuenta'),
+    path('user/eliminarCuenta', views.eliminarCuenta, name='eliminarCuenta'),
+
+    path('repartidor/pendientes', views.pedidosPendientes, name='pedidosPendientes'),
+    path('repartidor/pedido', views.pedidoRepartidor, name='pedidoRepartidor'),
+
+    path('restaurante/ordenes', views.ordenes, name='ordenesPendientes'),
+
 ]
