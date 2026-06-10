e8f5985893d6648f82cc8139ddf3350b6572187f
WraithLion <wraithpuma@ciencias.unam.mx>
2026-06-10 07:52:25 -0600

    Implementacion de controladores para Restaurante

--- sitio_3C/app3C/urls.py
+++ sitio_3C/app3C/urls.py
@@ -11,7 +11,23 @@ urlpatterns = [
     path('crearCuenta/tipo', views.tipoCuenta, name='tipoCuenta'),
     path('crearCuenta/cliente', views.datosCliente, name='datosCliente'),
     path('crearCuenta/repartidor', views.datosRepartidor, name='datosRepartidor'),
+
     path('crearCuenta/restaurante', views.datosRestaurante, name='datosRestaurante'),
+
+    # Rutas para Platillos
+    path('restaurante/platillos/agregar/', views.agregar_platillo, name='agregar_platillo'),
+    # Nota: Para modificar y eliminar, se hará uso del ID en la URL.
+    # El frontend enviará POST a estas rutas con el ID.
+    path('restaurante/platillos/<str:id_elemento>/modificar/', views.modificar_platillo, name='modificar_platillo'),
+    path('restaurante/platillos/<str:id_elemento>/eliminar/', views.eliminar_platillo, name='eliminar_platillo'),
+
+    # Rutas para Promociones
+
+    path('restaurante/promociones/agregar/', views.agregar_promocion, name='agregar_promocion'),
+    path('restaurante/promociones/<int:promo_id>/modificar/', views.modificar_promocion, name='modificar_promocion'),
+    path('restaurante/promociones/eliminar/', views.eliminar_promocion, name='eliminar_promocion'),
+# # #     Tarjetas
+
     path('crearCuenta/tarjeta', views.datosTarjeta, name='datosTarjeta'),
     path('crearCuenta/vehiculo', views.datosVehiculo, name='datosVehiculo'),
 
